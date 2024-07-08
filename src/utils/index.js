c4da6efe01a58a4e738d8d192d4231ff7274983a
dejosemario <josemariaodumodu@gmail.com>
2024-07-08 16:23:13 +0100

    fix: validate req body against schema

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,16 +1,28 @@
+const { ValidationErrorItem } = require("sequelize");
 
-
-const wrapper = (fn) =>{
-    return async (req, res, next) =>{
-        try{
-            await fn(req, res, next);
-        }catch(err){
-            next(err);
-        }
+const wrapper = (fn) => {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (err) {
+      next(err);
     }
-}
+  };
+};
 
+//used to validate the request body against the schema
+const validate = (schema, body) => {
+  const { error } = schema.validate(body);
+  if (error) {
+    const errorMessage = error.details.map((err) => ({
+      field: err.context.key,
+      message: err.message,
+    }));
+    throw new Error(errorMessage);
+  }
+};
 
 module.exports({
-    wrapper
-})
+  wrapper,
+  validate,
+});
